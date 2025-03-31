4b32ecc50c12d51368eec78b1fa10b893a52c94f
William McPherson <willmcpherson2@gmail.com>
2025-03-31 22:50:47 +1100

    Add language servers

--- flake.nix
+++ flake.nix
@@ -2,13 +2,21 @@
   description = "Haskell 💞 Rust";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
@@ -20,7 +28,13 @@
           buildInputs = [
             pkgs.cabal-install
             pkgs.haskell.compiler.ghc910
-            pkgs.rust-bin.nightly."2025-03-31".default
+            (pkgs.haskell-language-server.override { supportedGhcVersions = [ "910" ]; })
+            (pkgs.rust-bin.nightly."2025-03-31".default.override {
+              extensions = [
+                "rust-src"
+                "rust-analyzer-preview"
+              ];
+            })
           ];
           shellHook = ''
             export LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
