178ce192e56e76b742df4d096aec7a805f85d7f6
William McPherson <willmcpherson2@gmail.com>
2025-04-04 19:48:27 +1100

    Upgrade to Rust 2024 edition

--- rs/lib.rs
+++ rs/lib.rs
@@ -6,21 +6,21 @@ pub struct Point {
 }
 
 impl Point {
-    #[no_mangle]
+    #[unsafe(no_mangle)]
     pub extern "C" fn new_point(x: f64, y: f64) -> Box<Point> {
         Box::new(Point { x, y })
     }
 
-    #[no_mangle]
+    #[unsafe(no_mangle)]
     pub extern "C" fn point_length(&self) -> f64 {
         (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
 
-    #[no_mangle]
+    #[unsafe(no_mangle)]
     pub extern "C" fn print_point(&self) {
         println!("{:?}", self)
     }
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn free_point(_point: Box<Point>) {}
